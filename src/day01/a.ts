04deea08885c8a1291b9a888ceaeafb04fbc54e9
Roel Lakenvelt <roel@lakenvelt.nl>
2024-12-01 09:32:38 +0100

    Day 01

--- src/day01/a.ts
+++ src/day01/a.ts
@@ -1,14 +1,19 @@
 import InputHelper from '../utils/input';
 import Logger from '../utils/logger';
 
-const puzzle = 'Day 1A: '
+const puzzle = 'Day 1A: Historian Hysteria'
 const input = new InputHelper();
 const logger = new Logger(puzzle);
 const lines = input.getInput();
 
 logger.start();
 
-let answer = 0;
+const list1 = lines.map(line=>parseInt(line.split(' ')[0])).sort((a,b)=>a-b);
+const list2 = lines.map(line=>parseInt(line.split('   ')[1])).sort((a,b)=>a-b);
+
+let answer = list1.reduce((acc, curr, index) => {
+    return acc + Math.abs(curr - list2[index]);
+}, 0);
 
 logger.end(answer);
 
--- src/day01/b.ts
+++ src/day01/b.ts
@@ -1,14 +1,20 @@
 import InputHelper from '../utils/input';
 import Logger from '../utils/logger';
 
-const puzzle = 'Day 1B: '
+const puzzle = 'Day 1B: Historian Hysteria'
 const input = new InputHelper();
 const logger = new Logger(puzzle);
 const lines = input.getInput();
 
 logger.start();
 
-let answer = 0;
+const list1 = lines.map(line=>parseInt(line.split(' ')[0]))
+const list2 = lines.map(line=>parseInt(line.split('   ')[1]));
+
+let answer = list1.reduce((acc, curr) => {
+    const apearances = list2.filter(num => num === curr).length;
+    return acc + curr * apearances;
+}, 0);
 
 logger.end(answer);
 
