c479166c1d1fa0b09b1bd18b35bb1ed4f9fd28b8
Roel Lakenvelt <roel@lakenvelt.nl>
2024-12-03 07:50:55 +0100

    Day 03

--- src/day03/a.ts
+++ src/day03/a.ts
@@ -11,12 +11,12 @@ logger.start();
 
 const regex = /mul\((\d+),(\d+)\)/g;
 let match;
-let sum = 0;
+let answer = 0;
 
 while ((match = regex.exec(memory)) !== null) {
     const x = parseInt(match[1]);
     const y = parseInt(match[2]);
-    sum += x * y;
+    answer += x * y;
 }
 
-logger.end(sum);
+logger.end(answer);
