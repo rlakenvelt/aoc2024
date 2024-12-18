7fddb5214bc71e1997698c5dac2c69acb7f9ec47
Roel Lakenvelt <roel@lakenvelt.nl>
2024-12-18 20:05:43 +0100

    Day 15 part 2

--- src/day15/b.ts
+++ src/day15/b.ts
@@ -1,12 +1,15 @@
 import InputHelper from '../utils/input';
 import Logger from '../utils/logger';
+import { Point } from '../utils/point';
 
 const puzzle = 'Day 15B: Warehouse Woes'
 const input = new InputHelper();
 const logger = new Logger(puzzle);
 
 let inputValues = input.getInput('\n\n');
-const map = inputValues[0].split('\n')
+type Map = string[][];
+type Box = Point[];
+const map: Map = inputValues[0].split('\n')
                           .map(l => {
                                 let t = l.replace(/#/g, '##')
                                 t = t.replace(/O/g, '[]')
@@ -16,7 +19,6 @@ const map = inputValues[0].split('\n')
                           })
                           .map(l => l.split(''));
 
-// const map = inputValues[0].split('\n').map(l => l.split(''));
 const moves = inputValues[1].split('\n').join('').split('');
 
 logger.start();
@@ -31,11 +33,10 @@ const directions: { [key: string]: Direction } = {
 
 let robotPosition = findRobot(map);
 
-display(map);
 
 let answer = 0;
 
-moves.forEach((move) => {
+moves.forEach((move, index) => {
     robotPosition = moveRobot(map, robotPosition, move);
 });
 
@@ -49,7 +50,7 @@ for (let y = 0; y < map.length; y++) {
 
 logger.end(answer);
 
-function moveRobot(map: string[][], robotPosition: { x: number, y: number }, move: string): { x: number, y: number } {
+function moveRobot(map: Map, robotPosition: { x: number, y: number }, move: string): { x: number, y: number } {
     const direction = directions[move];
     const newX = robotPosition.x + direction.dx;
     const newY = robotPosition.y + direction.dy;
@@ -57,44 +58,61 @@ function moveRobot(map: string[][], robotPosition: { x: number, y: number }, mov
     if (map[newY][newX] === '#') {
         return robotPosition; 
     }
-
-    if (isBox(map[newY][newX])) {
-        if (!pushBox(map, newX, newY, direction)) {
+    const box = checkBox(map, new Point(newX, newY));
+    if (box) {
+        if (!pushBox(map, box, direction, true)) {
             return robotPosition; 
         }
-    }
-
+        pushBox(map, box, direction, false)
+    }   
     map[robotPosition.y][robotPosition.x] = '.';
     map[newY][newX] = '@';
     return { x: newX, y: newY };
 }
 
 
-function pushBox(map: string[][], x: number, y: number, direction: Direction): boolean {
-    const newX = x + direction.dx;
-    const newY = y + direction.dy;
+function pushBox(map: Map, box: Box, direction: Direction, simulate: boolean): boolean {
+    for (let i = 0; i < box.length; i++) {
+        const newX = box[i].x + direction.dx;
+        const newY = box[i].y + direction.dy;
+        if (map[newY][newX] === '#') {
+            return false; 
+        }
+        const neighborBox = checkBox(map, new Point(newX, newY));
+        if (neighborBox && 
+            (neighborBox[0].x !== box[0].x || neighborBox[0].y !== box[0].y) &&
+            !pushBox(map, neighborBox, direction, simulate)) {
+            return false; 
+        }
+    };
+    if (!simulate) {
+        if (direction.dx !== 0) { 
+            map[box[0].y][box[0].x + direction.dx] = '[';
+            map[box[0].y][box[1].x + direction.dx] = ']';
+        } else {
+            map[box[0].y + direction.dy][box[0].x] = '[';
+            map[box[0].y + direction.dy][box[1].x] = ']';
+            map[box[0].y][box[0].x] = '.';
+            map[box[0].y][box[1].x] = '.';
 
-    if (map[newY][newX] === '#' || (isBox(map[newY][newX]) && !pushBox(map, newX, newY, direction))) {
-        return false; 
-    }
-    if (direction.dx !== 0) { 
-        map[newY + direction.dy][newX + direction.dx] = map[newY][newX];
-        map[newY][newX ] = map[y][x];
-        map[y][x] = '.';
-    } else { 
-        map[newY][newX] = '[';
-        map[newY + 1][newX] = ']';
-        map[y][x] = '.';
-        map[y + 1][x] = '.';
-    }
+        }
+ }
+
     return true;
+
 }
 
-function isBox(value: string): boolean {
-    return value === '[' || value === ']';
+function checkBox(map: Map, location: Point): Box | undefined {
+    if (map[location.y][location.x] === '[') {
+        return [location, new Point(location.x + 1, location.y)];
+    }
+    if (map[location.y][location.x] === ']') {
+        return [new Point(location.x - 1, location.y), location];
+    }
+    return undefined;
 }
 
-function findRobot(map: string[][]): { x: number, y: number } {
+function findRobot(map: Map): { x: number, y: number } {
     for (let y = 0; y < map.length; y++) {
         for (let x = 0; x < map[y].length; x++) {
             if (map[y][x] === '@') {
@@ -111,4 +129,3 @@ function display(map: string[][]) {
         console.log(temp);
     })
 } 
-
