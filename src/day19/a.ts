7d94a052217e2ce900cdba3612cd4747ecf77e4f
Roel Lakenvelt <roel@lakenvelt.nl>
2024-12-19 09:40:27 +0100

    Day 19

---
+++ src/day19/a.ts
@@ -0,0 +1,36 @@
+import InputHelper from '../utils/input';
+import Logger from '../utils/logger';
+
+const puzzle = 'Day 19A: Linen Layout'
+const input = new InputHelper();
+const logger = new Logger(puzzle);
+
+const inputValues = input.getInput('\n\n');
+const towels = inputValues[0].split(',').map(towel => towel.trim()); 
+const patterns = inputValues[1].split('\n');
+
+logger.start();
+let answer = 0;
+
+patterns.forEach(pattern => {
+    if (canConstructPattern(pattern, towels)) {
+        answer++;
+    }
+});
+
+logger.end(answer);
+
+function canConstructPattern(pattern: string, towels: string[]): boolean {
+    const canConstruct = Array(pattern.length + 1).fill(false);
+    canConstruct[0] = true;
+
+    for (let i = 1; i <= pattern.length; i++) {
+        for (let towel of towels) {
+            if (i >= towel.length && pattern.slice(i - towel.length, i) === towel) {
+                canConstruct[i] = canConstruct[i] || canConstruct[i - towel.length];
+            }
+        }
+    }
+
+    return canConstruct[pattern.length];
+}
---
+++ src/day19/b.ts
@@ -0,0 +1,36 @@
+import InputHelper from '../utils/input';
+import Logger from '../utils/logger';
+
+const puzzle = 'Day 19B: Linen Layout'
+const input = new InputHelper();
+const logger = new Logger(puzzle);
+
+const inputValues = input.getInput('\n\n');
+const towels = inputValues[0].split(',').map(towel => towel.trim()); 
+const patterns = inputValues[1].split('\n');
+
+logger.start();
+let answer = 0;
+
+patterns.forEach(pattern => {
+    answer+=numberOfConstructs(pattern, towels)
+});
+
+logger.end(answer);
+
+function numberOfConstructs(pattern: string, towels: string[]): number {
+    const canConstruct = Array(pattern.length + 1).fill(0);
+    canConstruct[0] = 1;
+
+    for (let i = 1; i <= pattern.length; i++) {
+        for (let towel of towels) {
+            if (i >= towel.length && pattern.slice(i - towel.length, i) === towel) {
+                if (canConstruct[i - towel.length] > 0) {
+                    canConstruct[i] = canConstruct[i] + canConstruct[i - towel.length];
+                }
+            }
+        }
+    }
+
+    return canConstruct[pattern.length];
+}
